cfa696370d3487e4a2762292e1e3930834d8a834
Brian M. Workman <1266935+gesslar@users.noreply.github.com>
2025-11-14 01:09:13 -0500

    delete dead files

--- adm/daemons/test.test.c
+++
@@ -1,9 +0,0 @@
-string test = "hi  there    this has a lot    of spaces    .";
-
-text = reduce(explode(trim(test), ""), function(string acc, string char, int index, string *chars) {
-  if(char == " " && chars[index - 1] == " ")
-    return acc;
-
-  return acc + char;
-
-}, "");
